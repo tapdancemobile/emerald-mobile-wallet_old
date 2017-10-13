3ba22f7811f2ff0cc6ab34d623a89064ad28db4b
Mark Mathis <markmathis@gmail.com>
2017-10-13 09:57:36 -0400

    Added barebones readme

--- rust/emerald_mobile_wallet/target/armv7-linux-androideabi/release/libemerald_mobile_wallet.d
+++
@@ -1,1 +0,0 @@
-/Users/macuser/Projects/RN/emerald_mobile_wallet/rust/emerald_mobile_wallet/target/armv7-linux-androideabi/release/libemerald_mobile_wallet.so: /Users/macuser/Projects/RN/emerald_mobile_wallet/rust/emerald_mobile_wallet/src/string.rs /Users/macuser/Projects/RN/emerald_mobile_wallet/rust/emerald_mobile_wallet/src/lib.rs
--- rust/emerald_mobile_wallet/target/i686-linux-android/release/libemerald_mobile_wallet.d
+++
@@ -1,1 +0,0 @@
-/Users/macuser/Projects/RN/emerald_mobile_wallet/rust/emerald_mobile_wallet/target/i686-linux-android/release/libemerald_mobile_wallet.so: /Users/macuser/Projects/RN/emerald_mobile_wallet/rust/emerald_mobile_wallet/src/string.rs /Users/macuser/Projects/RN/emerald_mobile_wallet/rust/emerald_mobile_wallet/src/lib.rs
