a6e401695d938e7a0a9a8b843db0fe0fe00b9b42
Mark Mathis <markmathis@gmail.com>
2017-10-25 15:52:03 -0400

    Removed the GPL helper file string.rs - Only Android at this time - should be able to use the Rust String wrapper instead of the removed one - https://medium.com/@redrield/the-stringptr-struct-under-src-string-rs-3d3f92f518d1 - Once I do this the app will run on iOS as well

--- rust/emerald_mobile_wallet/src/lib.rs
+++ rust/emerald_mobile_wallet/src/lib.rs
@@ -1,33 +1,3 @@
-extern crate libc;
-
-mod string;
-
-use string::StringPtr;
-
-// string ffi
-
-#[no_mangle]
-pub unsafe extern fn rust_string_ptr(s: *mut String) -> *mut StringPtr {
-  Box::into_raw(Box::new(StringPtr::from(&**s)))
-}
-
-#[no_mangle]
-pub unsafe extern fn rust_string_destroy(s: *mut String) {
-  let _ = Box::from_raw(s);
-}
-
-#[no_mangle]
-pub unsafe extern fn rust_string_ptr_destroy(s: *mut StringPtr) {
-  let _ = Box::from_raw(s);
-}
-
-#[no_mangle]
-pub unsafe extern fn welcome_emerald(platform: *mut StringPtr) -> *mut String {
-  let platform = (*platform).as_str();
-  let response = format!("Emerald Wallet is Rusty on {}", platform);
-  Box::into_raw(Box::new(response))
-}
-
 #[cfg(feature = "jni")]
 #[allow(non_snake_case)]
 pub mod android {
